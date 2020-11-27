4cf216968a088dd5ea927545791da1f240079ceb
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2020-11-26 19:08:13 -0600

    validar fecha

--- src/schedulecalendar/SchedulePage.js
+++ src/schedulecalendar/SchedulePage.js
@@ -11,90 +11,97 @@ import 'react-calendar/dist/Calendar.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 export default function SchedulePage() {
-  // context
-  const sessionContext = useContext(SessionContext);
-  const { id } = sessionContext.user;
+    // context
+    const sessionContext = useContext(SessionContext);
+    const { id } = sessionContext.user;
 
-  // hooks
-  const [refresch, setRefresch] = useState(false);
+    // hooks
+    const [refresch, setRefresch] = useState(false);
 
-  // custom hooks
-  const [evento, handleChangeEvento] = useEvento();
-  const [detalles, handleChangeDetalles] = useDetalles();
-  const [date, handleChangeDate] = useDate();
-  const [eventos, setDateToFetch] = useFetch(id, date, refresch, setRefresch);
-  const [postData] = usePostData();
+    // custom hooks
+    const [evento, handleChangeEvento] = useEvento();
+    const [detalles, handleChangeDetalles] = useDetalles();
+    const [date, handleChangeDate] = useDate();
+    const [eventos, setDateToFetch] = useFetch(id, date, refresch, setRefresch);
+    const [postData] = usePostData();
 
-  // object destructuring
-  const { day, month, year } = date;
-  const { loading: isLoading, data } = eventos;
+    // object destructuring
+    const { day, month, year } = date;
+    const { loading: isLoading, data } = eventos;
 
-  // effects
-  useEffect(() => {
-    setDateToFetch(date);
-  }, [date, setDateToFetch]);
+    // effects
+    useEffect(() => {
+        setDateToFetch(date);
+    }, [date, setDateToFetch]);
 
-  // handlers
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const body = {
-      evento,
-      detalles,
-      date,
-      userId: id,
+    // handlers
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const body = {
+            evento,
+            detalles,
+            date,
+            userId: id,
+        };
+
+        postData(body, date);
+        setRefresch(true);
     };
 
-    postData(body);
-    setRefresch(true);
-  };
+    return ( <
+        div className = "container" > { /* una barra de navegacion que permita volver */ } <
+        div className = "row" >
+        <
+        div className = "col-sm-4" >
+        <
+        Calendar onClickDay = {
+            (e) => {
+                handleChangeDate(e);
+            }
+        }
+        /> <
+        /div> <
+        div className = "col-sm-8" >
+        <
+        h2 > Agregar nuevo evento: { `${day}-${month + 1}-${year}` } < /h2> <
+        hr / >
 
-  return (
-    <div className="container">
-      {/* una barra de navegacion que permita volver */}
-      <div className="row">
-        <div className="col-sm-4">
-          <Calendar
-            onClickDay={(e) => {
-              handleChangeDate(e);
-            }}
-          />
-        </div>
-        <div className="col-sm-8">
-          <h2>Agregar nuevo evento: {`${day}-${month + 1}-${year}`}</h2>
-          <hr />
+        <
+        form onSubmit = { handleSubmit } >
+        <
+        div className = "form-group" >
+        <
+        label htmlFor = "evento" > Evento < /label> <
+        input type = "text"
+        className = "form-control"
+        id = "evento"
+        onChange = { handleChangeEvento }
+        /> <
+        /div>
 
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="evento">Evento</label>
-              <input
-                type="text"
-                className="form-control"
-                id="evento"
-                onChange={handleChangeEvento}
-              />
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="task">Detalles</label>
-              <textarea
-                className="form-control"
-                id="task"
-                rows="10"
-                onChange={handleChangeDetalles}
-              ></textarea>
-            </div>
-            <button type="submit" className="btn btn-primary">
-              Guardar
-            </button>
-          </form>
-        </div>
-      </div>
-      <div className="container-sm">
-        <h2>Eventos agendados</h2>
-        <hr />
-        {isLoading && <p>cargando...</p>}
-        {/* aqui van los eventos creen un componente */}
-      </div>
-    </div>
-  );
-}
+        <
+        div className = "form-group" >
+        <
+        label htmlFor = "task" > Detalles < /label> <
+        textarea className = "form-control"
+        id = "task"
+        rows = "10"
+        onChange = { handleChangeDetalles } >
+        < /textarea> <
+        /div> <
+        button type = "submit"
+        className = "btn btn-primary" >
+        Guardar <
+        /button> <
+        /form> <
+        /div> <
+        /div> <
+        div className = "container-sm" >
+        <
+        h2 > Eventos agendados < /h2> <
+        hr / > {
+            isLoading && < p > cargando... < /p>} { /* aqui van los eventos creen un componente */ } <
+            /div> <
+            /div>
+        );
+    }
--- src/schedulecalendar/hooks/usePostData.js
+++ src/schedulecalendar/hooks/usePostData.js
@@ -1,20 +1,37 @@
-
 export default function usePostData() {
-  function postData(body) {
-    fetch(`http://localhost:7000/api/evento/`, {
-      method: 'POST',
-      headers: {
-        Authorization: body.userId,
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    })
-      .then((resp) => resp.json())
-      .then((data) => {
-        console.log(data);
-      });
-  }
+    function postData(body, date) {
+        const { day, month, year } = date;
+        const fecha = `${year}-${month}-${day}`;
+        var fecha_hoy = new Date();
+        var fechaForm = new Date(year, month, day);
+        fecha_hoy.setHours(0, 0, 0, 0);
+        console.log(date);
 
-  return [postData];
+        alert(fecha);
+
+        if (fechaForm >= fecha_hoy) {
+            console.log("Fecha correcta");
+            alert("Fecha correcta");
+            fetch(`http://localhost:7000/api/evento/`, {
+                    method: 'POST',
+                    headers: {
+                        Authorization: body.userId,
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(body),
+                }).then((resp) => resp.json())
+                .then((data) => {
+                    console.log(data);
+                });
+        } else {
+            console.log(fechaForm);
+            console.log(" es una fecha incorrecta");
+            alert("Fecha incorrecta");
+        }
+
+    }
+
+    return [postData];
+
 }
