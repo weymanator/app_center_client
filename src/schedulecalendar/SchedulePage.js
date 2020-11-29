0994c5a3cdc070a35be9f4ca0e2c845260e60e9d
imangolf1313 <osvaldo.lopez.is@unipolidgo.edu.mx>
2020-11-28 20:36:55 -0600

    Eventos registrados

--- src/schedulecalendar/SchedulePage.js
+++ src/schedulecalendar/SchedulePage.js
@@ -1,14 +1,14 @@
-import React, { useContext, useEffect, useState } from 'react';
-import Calendar from 'react-calendar';
-import useDetalles from './hooks/useDetalles';
-import useDate from './hooks/useDate';
-import SessionContext from '../globals/SessionContext';
-import useFetch from './hooks/useFetch';
-import useEvento from './hooks/useEvento';
-import usePostData from './hooks/usePostData';
+import React, { useContext, useEffect, useState } from "react";
+import Calendar from "react-calendar";
+import useDetalles from "./hooks/useDetalles";
+import useDate from "./hooks/useDate";
+import SessionContext from "../globals/SessionContext";
+import useFetch from "./hooks/useFetch";
+import useEvento from "./hooks/useEvento";
+import usePostData from "./hooks/usePostData";
 
-import 'react-calendar/dist/Calendar.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import "react-calendar/dist/Calendar.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function SchedulePage() {
     // context
@@ -48,60 +48,83 @@ export default function SchedulePage() {
         setRefresch(true);
     };
 
-    return ( <
-        div className = "container" > { /* una barra de navegacion que permita volver */ } <
-        div className = "row" >
-        <
-        div className = "col-sm-4" >
-        <
-        Calendar onClickDay = {
-            (e) => {
-                handleChangeDate(e);
-            }
-        }
-        /> <
-        /div> <
-        div className = "col-sm-8" >
-        <
-        h2 > Agregar nuevo evento: { `${day}-${month + 1}-${year}` } < /h2> <
-        hr / >
-
-        <
-        form onSubmit = { handleSubmit } >
-        <
-        div className = "form-group" >
-        <
-        label htmlFor = "evento" > Evento < /label> <
-        input type = "text"
-        className = "form-control"
-        id = "evento"
-        onChange = { handleChangeEvento }
-        /> <
-        /div>
-
-        <
-        div className = "form-group" >
-        <
-        label htmlFor = "task" > Detalles < /label> <
-        textarea className = "form-control"
-        id = "task"
-        rows = "10"
-        onChange = { handleChangeDetalles } >
-        < /textarea> <
-        /div> <
-        button type = "submit"
-        className = "btn btn-primary" >
-        Guardar <
-        /button> <
-        /form> <
-        /div> <
-        /div> <
-        div className = "container-sm" >
-        <
-        h2 > Eventos agendados < /h2> <
-        hr / > {
-            isLoading && < p > cargando... < /p>} { /* aqui van los eventos creen un componente */ } <
-            /div> <
-            /div>
-        );
-    }
+    return (
+        <div className="container">
+            {/* una barra de navegacion que permita volver */}
+            <div className="row">
+                <div className="col-sm-4">
+                    <Calendar
+                        onClickDay={(e) => {
+                            handleChangeDate(e);
+                        }}
+                    />
+                </div>
+                <div className="col-sm-8">
+                    <h2>
+                        Agregar nuevo evento: {`${day}-${month + 1}-${year}`}
+                    </h2>
+                    <hr />
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-group">
+                            <label htmlFor="evento"> Evento </label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="evento"
+                                onChange={handleChangeEvento}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="task"> Detalles </label>
+                            <textarea
+                                className="form-control"
+                                id="task"
+                                rows="10"
+                                onChange={handleChangeDetalles}
+                            ></textarea>
+                        </div>
+                        <button type="submit" className="btn btn-primary">
+                            Guardar
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <div className="container-sm">
+                <h2> Eventos agendados </h2> <hr />
+                {isLoading && <p> cargando... </p>}
+                {
+                    /* aqui van los eventos creen un componente*/
+                    <ul>
+                        {!!data &&
+                            data.map((d) => (
+                                <section>
+                                    <li key={d.id}>
+                                        <div
+                                            className="titulo"
+                                            style={{
+                                                height: 80,
+                                                width: "100%",
+                                                display: "flex",
+                                                alignItems: "center",
+                                                justifyContent: "space-evenly",
+                                            }}
+                                        >
+                                            <h3>Evento: </h3> {d.evento}
+                                            <button className="btn btn-primary">
+                                                Eliminar
+                                            </button>
+                                        </div>
+                                        <div className="cuerpo">
+                                            <h4>Detalles: </h4> {d.detalles}
+                                            <h4>Fecha: </h4> {d.fecha}
+                                        </div>
+                                        <hr />
+                                    </li>
+                                </section>
+                            ))}
+                    </ul>
+                }
+            </div>
+        </div>
+    );
+}
